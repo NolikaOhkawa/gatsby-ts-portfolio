4974755f29652ef6894b17b751f2b7cbe94d27a9
Norika Okawa <n.okawa@public-id.com>
2024-05-01 15:26:38 -0700

    first commit

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,20 +1,40 @@
-import type { GatsbyConfig } from "gatsby";
+import type { GatsbyConfig } from 'gatsby';
 
 const config: GatsbyConfig = {
   siteMetadata: {
     title: `gatsby-ts-site`,
-    siteUrl: `https://www.yourdomain.tld`
+    siteUrl: `https://www.yourdomain.tld`,
   },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
-  plugins: [{
-    resolve: 'gatsby-source-wordpress',
-    options: {
-      "url": "https://norika-oh.com"
-    }
-  }, "gatsby-plugin-image", "gatsby-plugin-sharp", "gatsby-transformer-sharp", "gatsby-plugin-postcss", "gatsby-plugin-google-gtag"]
+  plugins: [
+    {
+      resolve: 'gatsby-source-wordpress',
+      options: {
+        url: 'https://norika-oh.com/graphql',
+      },
+    },
+    'gatsby-plugin-image',
+    'gatsby-plugin-sharp',
+    'gatsby-transformer-sharp',
+    'gatsby-plugin-postcss',
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        trackingIds: [
+          'G-X6HWHR5CBE',
+          // 'GA-TRACKING_ID', // Google Analytics / GA
+        ],
+        // このプラグインのオプションに関するその他の設定があれば追加
+        pluginConfig: {
+          // Google Analyticsがページをロードする前にヒットを送信しないようにします
+          respectDNT: true,
+        },
+      },
+    },
+  ],
 };
 
 export default config;
